52f2deea36749471caa69aa966ed68ab99f5f4aa
sanjay2210 <backton2022@gmail.com>
2020-01-09 12:40:20 +0000

    new

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ var express = require("express"),
 //requring routescd 
 ////
 const forceSecure = require("force-secure-express");
-app.use(forceSecure([
-    "vitconnex.com",
-    "www.vitconnex.com"
-]));
+
 app.use(forceSecure([
     "www.vitconnex.com",
     "https://www.vitconnex.com"
