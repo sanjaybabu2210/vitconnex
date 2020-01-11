17b5fc97fd4a23ec6e8e0de1601858d5ce9e2a6a
sanjay2210 <backton2022@gmail.com>
2020-01-11 04:52:19 +0000

    new

--- routes/index.js
+++ routes/index.js
@@ -81,6 +81,9 @@ router.post("/step3",function(req,res){
 router.get("/register", function(req,res){
 	res.render("register",{page: 'register'});
 })
+router.get("/sitemap", function(req,res){
+	res.render("sitemap");
+})
 router.get("/register/admin", function(req,res){
 	res.render("admin",{page: 'register'});
 })
